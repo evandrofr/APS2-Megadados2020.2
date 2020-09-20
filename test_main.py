acf83f9c49c7c7ad7b70569ec4ddd5493615d928
evandrofr <38050761+evandrofr@users.noreply.github.com>
2020-09-20 18:35:02 -0300

    Add main_test.py

---
+++ test_main.py
@@ -0,0 +1,7 @@
+from fastapi.testclient import TestClient
+from main import app
+client = TestClient(app)
+def test_read_main_returns_not_found():
+ response = client.get('/')
+ assert response.status_code == 404
+ assert response.json() == {'detail': 'Not Found'}
