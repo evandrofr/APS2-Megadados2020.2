4d42f13dcc1d471833c860ebb0aa5063600eb709
evandrofr <38050761+evandrofr@users.noreply.github.com>
2020-09-20 22:16:47 -0300

    Testes funcionando

--- test_main.py
+++ test_main.py
@@ -48,13 +48,13 @@ def test_delete_invalid_task():
 
 def test_read_all_tasks():
     responsePost1 = client.post('/task', json={"description": "description1", "completed": False})
-    assert responsePost.status_code == 200
+    assert responsePost1.status_code == 200
 
     responsePost2 = client.post('/task', json={"description": "description2", "completed": True})
-    assert responsePost.status_code == 200
+    assert responsePost2.status_code == 200
 
     responsePost3 = client.post('/task', json={"description": "description3", "completed": False})
-    assert responsePost.status_code == 200
+    assert responsePost3.status_code == 200
 
     responseGet = client.get('/task')
     assert responseGet.status_code == 200
@@ -69,39 +69,39 @@ def test_read_all_tasks():
         },
         responsePost3.json(): {
             "description": "description3",
-            "completed": True,
+            "completed": False,
         },
     }
-    responseDelete1 = client.delete(f"/task/{response1.json()}")
+    responseDelete1 = client.delete(f"/task/{responsePost1.json()}")
     assert responseDelete1.status_code == 200
     assert responseDelete1.json() == None
-    responseDelete2 = client.delete(f"/task/{response2.json()}")
+    responseDelete2 = client.delete(f"/task/{responsePost2.json()}")
     assert responseDelete2.status_code == 200
     assert responseDelete2.json() == None
-    responseDelete3 = client.delete(f"/task/{response3.json()}")
+    responseDelete3 = client.delete(f"/task/{responsePost3.json()}")
     assert responseDelete3.status_code == 200
     assert responseDelete3.json() == None
 
 def test_patch_invalid_task():
     uuid_ = uuid.uuid4()
-    response = client.patch(f"/task/{uuid_}", json={"description": "description", "completed": "False"})
+    response = client.patch(f"/task/{uuid_}", json={"description": "description", "completed": False })
     assert response.status_code == 404
     assert response.json() == {"detail": "Task not found"}
 
 def test_patch_valid_task():
     responsePost = client.post(
-        "/task", json={"description": "description1", "completed": "False"}
+        "/task", json={"description": "description1", "completed": False }
     )
     assert responsePost.status_code == 200
 
     responsePatch = client.patch(
         f"/task/{responsePost.json()}",
-        json={"description": "description2", "completed": "True"},
+        json={"description": "description2", "completed": True },
     )
     assert responsePatch.status_code == 200
     assert responsePatch.json() == None
 
-    responseGet = client.get(f"/task/{response.json()}")
+    responseGet = client.get(f"/task/{responsePost.json()}")
     assert responseGet.status_code == 200
     assert responseGet.json() == {
         "description": "description2",
@@ -115,19 +115,16 @@ def test_patch_valid_task():
 
 def test_put_valid_task():
 
-    responsePost = client.post("/task", json={"description": "description1", "completed": "True"})
+    responsePost = client.post("/task", json={"description": "description1", "completed": True })
     assert responsePost.status_code == 200
 
-    responsePut = client.put(f"/task/{responsePost.json()}",json={"description": "description2", "completed": "False"},)
+    responsePut = client.put(f"/task/{responsePost.json()}",json={"description": "description2", "completed": False },)
     assert responsePut.status_code == 200
     assert responsePut.json() == None
 
     responseGet = client.get(f"/task/{responsePost.json()}")
-    assert response3.status_code == 200
-    assert response3.json() == {
-        "description": "description2",
-        "completed": False,
-    }
+    assert responseGet.status_code == 200
+    assert responseGet.json() == { "description": "description2", "completed": False }
 
     responseDelete = client.delete(f"/task/{responsePost.json()}")
     assert responseDelete.status_code == 200
